533497f8b56129904427bdd8b95ab09108cf8005
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2025-06-18 14:31:34 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Teste-PokeAPI',
+  webDir: 'www'
+};
+
+export default config;
